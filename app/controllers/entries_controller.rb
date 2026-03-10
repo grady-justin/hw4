2996494230341e13feef870b8626dbe7c17d0eab
grady-justin <justin.grady@kellogg.northwestern.edu>
2026-03-10 22:54:16 +0000

    updates

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,6 +12,9 @@ class EntriesController < ApplicationController
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = @current_user["id"]
+    if params["uploaded_image"].present?
+      @entry.uploaded_image.attach(params["uploaded_image"])
+    end
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
